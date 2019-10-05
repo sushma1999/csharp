b9c3f6883b2415545e7a36e5256aafef85eabfa4
sushma1999 <56186986+sushma1999@users.noreply.github.com>
2019-10-05 12:54:20 +0530

    Initial commit

---
+++ csharp/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace csharp
+{
+    class Person
+    {
+        public string Fname;
+        public string Lname;
+        public void Intro()
+        {
+            Console.WriteLine("I am " +Fname +" " +Lname);
+        }
+    }
+}
---
+++ csharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.Fname = "Sush";
+            person.Lname = "Ephrin";
+            person.Intro();
+        }
+    }
+}
